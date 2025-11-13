6d7fee1ff196484764d6d031f1ed4541a153e2d5
Tastror <yccozy2001@163.com>
2025-11-14 01:44:51 +0800

    chore: delete doc data

--- logger.py
+++ logger.py
@@ -6,23 +6,12 @@ def setup_logger(level=logging.ERROR):
 
     logger = logging.getLogger()
 
-    # logging.DEBUG,
-    # logging.INFO,
-    # logging.WARNING,
-    # logging.ERROR,
-    # logging.CRITICAL
+    # DEBUG, INFO, WARNING, ERROR, CRITICAL
     logger.setLevel(level)
     if logger.hasHandlers():
         return logger
     handler = colorlog.StreamHandler(sys.stdout)
 
-    # %(log_color)s 是 colorlog 特有的，用来添加颜色
-    # %(levelname)-8s: 级别名，左对齐，占8个字符
-    # %(asctime)s: 时间
-    # %(module)s: 模块名
-    # %(funcName)s: 函数名
-    # %(lineno)d: 行号
-    # %(message)s: 日志信息
     log_format = (
         '%(log_color)s%(levelname)-8s '
         '%(asctime)s | '
